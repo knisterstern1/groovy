83f43a25828f65a439a5aed84e3d3dc8fc98a0e6
knisterstern1 <christian.steiner2@bs.ch>
2025-03-19 11:13:04 +0100

    Update Obj_ObjAccessoryRef.groovy
    method added to function

--- Obj_ObjAccessoryRef.groovy
+++ Obj_ObjAccessoryRef.groovy
@@ -1,8 +1,32 @@
-def outputArray = []
+def getMeasure(Object measure) {
+    def measureParts = []
+    def label = measure?.MeasureType3DimVoc
+    if (measure?.FirstNum) {
+      measureParts << measure?.FirstNum
+    }
+    if (measure?.SecondNum) {
+      measureParts << measure?.SecondNum
+    }
+     if (measure?.ThirdNum) {
+      measureParts << measure?.ThirdNum
+    }
+  
+    def measures = measureParts.join(" x ")
+    return [label, measures].join(": ") 
+}
 
+def outputArray = []
 def accessories = record?.ObjAccessoryRef
 
 accessories.each{
-
-  output = it?.AccNumberTxt
+  def item = []
+  if (it?.AccDenominationVoc) {
+    item << it?.AccDenominationVoc 
+  }
+  item << it?.AccNumberTxt
+  if (it?.AccDimAllGrp.size() > 0){
+    item << getMeasure(it?.AccDimAllGrp.findAll().first()) 
+  }
+  outputArray << item.join(", ")
 }
+output = outputArray.join("\n")
