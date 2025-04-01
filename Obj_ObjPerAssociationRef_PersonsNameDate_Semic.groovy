ba87b3d58ad41520e9519ab9843789d80b9aad31
knisterstern1 <christian.steiner2@bs.ch>
2025-04-01 10:32:47 +0200

    Update Obj_ObjPerAssociationRef_PersonsNameDate_Semic.groovy
    All entries, separated by semicolon
    •	For person records of type = ‘Einzelperson’: [Titel] [Vorname] [Nachname] 
    \[[Zuschreibung]\] [(Lebensdaten)]
    •	For person records of type = ‘Organisation’: [Organisation] \[[Zuschreibung]\], [(Bestehen)]
    •	For person records of type = ‘Personengruppe’: [Name] \[[Zuschreibung]\] [(Bestehen)]

--- Obj_ObjPerAssociationRef_PersonsNameDate_Semic.groovy
+++ Obj_ObjPerAssociationRef_PersonsNameDate_Semic.groovy
@@ -1,30 +1,33 @@
 def outputArray = []
-
 def myList = fetch('ObjPerAssociationRef')?.collect()
 def sortedList = myList?.size() ? myList?.sort{a,b -> fetch('[SortLnu]', null, a) <=> fetch('[SortLnu]', null, b)} : null
 
 sortedList?.each{
+	def attribution = fetch('[AttributionVoc]', null, it)
 	switch(fetch('PerTypeVoc', null, it)?.logicalName){
 		case 'artist':
 			outputArray << combine(
-        fetch('PerTitleTxt', null, it), ' ',
+                                fetch('PerTitleTxt', null, it), ' ',
 				fetch('PerForeNameTxt', null, it), ' ',
 				fetch('PerSurNameTxt', null, it), ' ',
-				fetch('PerDatingTxt', null, it) ? '(' + fetch('PerDatingTxt', null, it) + ')':''
+                                attribution ? [attribution] : null, ' ',
+				fetch('PerDatingTxt', null, it) ? '(' + fetch('PerDatingTxt', null, it) + ')':''		
 			)
 		break
 		case 'organization':
 			outputArray << combine(
-				fetch('PerOrganisationMainBodyTxt', null, it), ', ',
-				fetch('PerDatingTxt', null, it) ? '(' + fetch('PerDatingTxt', null, it) + ')':''
+				fetch('PerOrganisationMainBodyTxt', null, it), ' ',
+                                attribution ? [attribution] : null, '',
+				fetch('PerDatingTxt', null, it) ? ', (' + fetch('PerDatingTxt', null, it) + ')':''
 			)
 		break
 		case 'group':
 			outputArray << combine(
 				fetch('PerNameTxt', null, it), ' ',
-				fetch('PerDatingTxt', null, it) ? '(' + fetch('PerDatingTxt', null, it) + ')':''
+                                attribution ? [attribution] : null, '',
+				fetch('PerDatingTxt', null, it) ? ', (' + fetch('PerDatingTxt', null, it) + ')':''
 			)
 		break
-  }
+     }
 }
 print outputArray?.join('; ')
